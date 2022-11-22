9c54b260e48aa98792c74a429d942781ba3dcdff
kiq-caique <caique-kiq@hotmail.com>
2022-11-22 12:34:54 -0300

    Component Pokemon

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,9 +5,30 @@ const Pokemon = (props) => {
   return (
     <div className="pokemon-card">
       <div className="pokemon-image-container">
-        <img alt={pokemon.name} src={pokemon.sprites.front_default} />
+        <img
+          alt={pokemon.name}
+          src={pokemon.sprites.front_default}
+          className="pokemon-image"
+        />
       </div>
-      {pokemon.name}
+      <div className="card-body">
+        <div className="card-top">
+          <div> #{pokemon.id}</div>
+          <h3> {pokemon.name} </h3>
+        </div>
+        <div className="card-botton">
+          <div className="pokemon-type">
+            {pokemon.types.map((type, index) => {
+              return (
+                <div key={index} className="pokemon-type-text">
+                  {type.type}
+                </div>
+              );
+            })}
+          </div>
+          <h3> {pokemon.name} </h3>
+        </div>
+      </div>
     </div>
   );
 };
